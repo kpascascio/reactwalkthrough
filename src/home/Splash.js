6146534eb84486506a20517f696a201e22d2795c
Kenn Pascascio <kenn370@gmail.com>
2018-04-12 09:44:03 -0400

    added files:

---
+++ src/home/Splash.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WorkoutlogIndex from '../workouts/WorkoutIndex'
+
+const Splash = (props) => {
+    return (
+        <div>
+            welcome user
+            <WorkoutlogIndex />
+        </div>
+
+    )
+}
+
+export default Splash;
---
+++ src/workouts/WorkoutIndex.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class WorkoutIndex extends React.Component {
+
+    render() {
+        return (
+            <div>WorkoutLog Index Component</div>
+        )
+    }
+}
+
+export default  WorkoutIndex;
